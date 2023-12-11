fd4057b35a232ace54c779ec32ff04e3f7ae6606
yama_can <yama.can95@gmail.com>
2023-12-11 19:27:53 +0900

    Fix Main.cpp

--- AM Test/Main.cpp
+++ AM Test/Main.cpp
@@ -4,6 +4,6 @@ void Main(AM::Application& application)
 {
 
 	AM::Window& window1 = application.createWindow();
-	AM::Window& window1 = application.createWindow();
+	AM::Window& window2 = application.createWindow();
 
 }
